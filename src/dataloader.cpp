d642f337d4270b29531bd72eb31573a1189c8d62
Antonio Skorin <antonio.skorin@gmail.com>
2026-06-28 20:27:23 -0400

    Feat: dataloader permite n>100

--- src/dataloader.cpp
+++ src/dataloader.cpp
@@ -1,4 +1,5 @@
 #include "../inc/dataloader.hpp"
+#include <cstring>
 
 DataLoader::DataLoader(int num_images, const std::string& dataset_dir, int target_width, int target_height)
     : dataset_dir(dataset_dir), num_images(num_images),
@@ -21,26 +22,37 @@ DataLoader::~DataLoader() {
     }
 }
 
+
 void DataLoader::preprocess_data() {
     if (is_loaded) return;
 
+    // Hardcodedo
+    const int actual_files_on_disk = 100; 
+
     for (int k = 1; k <= num_images; k++) {
-        std::string filename = dataset_dir + "/0" + std::to_string(800 + k) + "x4.png";
-        
-        CImg<unsigned char> img(filename.c_str());
-        
-        // Convertimos a greyscale
-        CImg<unsigned char> gray_img = img.spectrum() > 1 ? img.get_RGBtoYCbCr().channel(0) : img;
-
-        // Downscaling a target_width y height
-        gray_img.resize(target_width, target_height, -100, -100, 5); // 5 es bicubic interpolation
-
-        // Populamos el dataset
         size_t offset = static_cast<size_t>(k - 1) * elements_per_image;
         float* current_image_ptr = h_dataset + offset;
 
-        cimg_forXY(gray_img, x, y) {
-            current_image_ptr[y * target_width + x] = static_cast<float>(gray_img(x, y));
+        if (k <= actual_files_on_disk) {
+            // I/O y procesamiento CImg, esto corre 100 veces
+            // *: DIV2K parte de 0801
+            std::string filename = dataset_dir + "/0" + std::to_string(800 + k) + "x4.png";
+            
+            CImg<unsigned char> img(filename.c_str());
+            CImg<unsigned char> gray_img = img.spectrum() > 1 ? img.get_RGBtoYCbCr().channel(0) : img;
+            gray_img.resize(target_width, target_height, -100, -100, 5);
+
+            cimg_forXY(gray_img, x, y) {
+                current_image_ptr[y * target_width + x] = static_cast<float>(gray_img(x, y));
+            }
+        } else {
+            // Duplicación en memoria via modulo (corre para todas las imagenes restantes sobre 100)
+            // Calculamos el puntero fuente utilizando modulo para wrappear de vuelta a las primeras 100 imágenes
+            size_t source_offset = static_cast<size_t>((k - 1) % actual_files_on_disk) * elements_per_image;
+            float* source_ptr = h_dataset + source_offset;
+            
+            // memcpy!
+            std::memcpy(current_image_ptr, source_ptr, elements_per_image * sizeof(float));
         }
     }
     is_loaded = true;
